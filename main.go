132db22c1a01d9258e89abc8562eb837b0d8cd42
TriBogi <bogi.bobogi@gmail.com>
2022-11-24 13:32:29 +0700

    jwt auth, validate token

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
+
 	userHandler := handler.NewUserHandler(userService, authService)
 
 	router := gin.Default()
