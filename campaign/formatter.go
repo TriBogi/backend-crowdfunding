2491e153b483e2971ec1c6d61f705aa717b826e0
TriBogi <bogi.bobogi@gmail.com>
2022-11-30 17:13:47 +0700

    List API Campaign

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,43 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+	Slug             string `json:"slug"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	campaignFormatter := CampaignFormatter{}
+	campaignFormatter.ID = campaign.ID
+	campaignFormatter.UserID = campaign.User_ID
+	campaignFormatter.Name = campaign.Name
+	campaignFormatter.ShortDescription = campaign.Short_Description
+	campaignFormatter.GoalAmount = campaign.Goal_Amount
+	campaignFormatter.CurrentAmount = campaign.Current_Amount
+	campaignFormatter.Slug = campaign.Slug
+	campaignFormatter.ImageURL = ""
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageURL = campaign.CampaignImages[0].File_Name
+
+	}
+	return campaignFormatter
+
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignsFormatter := []CampaignFormatter{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+
+}
