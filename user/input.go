a8bd54f2cbe35f2d2448fe5e3f0d1b889143614f
TriBogi <bogi.bobogi@gmail.com>
2022-12-22 11:52:01 +0700

    eror handling user

--- user/input.go
+++ user/input.go
@@ -18,7 +18,8 @@ type CheckEmailInput struct {
 
 type FormCreateUserInput struct {
 	Name       string `form:"name" binding:"required"`
-	Email      string `form:"email" binding:"required"`
+	Email      string `form:"email" binding:"required,email"`
 	Occupation string `form:"occupation" binding:"required"`
 	Password   string `form:"password" binding:"required"`
+	Error      error
 }
--- web/handler/user.go
+++ web/handler/user.go
@@ -35,7 +35,9 @@ func (h *userHandler) Create(c *gin.Context) {
 
 	err := c.ShouldBind(&input)
 	if err != nil {
-		//skip
+		input.Error = err
+		c.HTML(http.StatusOK, "user_new.html", input)
+		return
 	}
 
 	registerInput := user.RegisterUserInput{}
@@ -46,7 +48,8 @@ func (h *userHandler) Create(c *gin.Context) {
 
 	_, err = h.userService.RegisterUser(registerInput)
 	if err != nil {
-		//skip
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
 	}
 
 	c.Redirect(http.StatusFound, "/users")
